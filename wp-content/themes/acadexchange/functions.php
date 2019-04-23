2c3174e32d49b22a98eb5cce633279a77e1c9f6e
vincentbulfon <zRD8w3ufrPdR>
2019-04-23 17:45:23 +0200

    added laravel-mix and header and footer files

--- wp-content/themes/acadexchange/functions.php
+++ wp-content/themes/acadexchange/functions.php
@@ -1,1 +1,25 @@
 <?php
+
+/** add custom thumbnails **/
+
+add_theme_support( 'post-thumbnails' );
+
+/** register nav menu **/
+
+register_nav_menu('main', 'navigation principale');
+register_nav_menu('footer', 'navigation secondaire');
+
+
+/** register custom post type **/
+
+function ae_register_post_types(){
+    register_post_type('services', [
+        'label' => 'services',
+        'labels' => ['add service' => 'ajouter un service'],
+        'description' => 'services disponibles sur le site',
+        'menu_position' => 5,
+        'menu_icon' => 'dashicons-editor-ul'
+    ]);
+}
+
+add_action('init', 'ae_register_post_types');
---
+++ wp-content/themes/acadexchange/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Acad exchange | Home</title>
+</head>
+<h1>
+	<header class="header">
+		<h1 class="header__logo">Acad exchange</h1>
+		<nav class="header__nav">
+			
+		</nav>
+	</header>
+    <body class="main">
